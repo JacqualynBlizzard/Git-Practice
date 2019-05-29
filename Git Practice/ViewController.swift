494c1912e1573d315836a73b5ac1df2465b5b62a
Jacqualyn Blizzard <jacqualyn.blizzard@gmail.com>
2019-05-29 13:12:27 -0500

    Update ViewController.swift
    I added an update so I could pull.

--- Git Practice/ViewController.swift
+++ Git Practice/ViewController.swift
@@ -11,12 +11,14 @@ import UIKit
 class ViewController: UIViewController {
     let message = "Hello Git!"
     let message2 = "This is my first branch"
+    let message3 = "This is an update for my first pull"
     @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         print(message)
         print(message2)
         myLabel.text = message
+        myLabel.text = message3
         // Do any additional setup after loading the view.
     }
 
